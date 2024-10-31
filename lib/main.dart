589659e81d8822ce021d1eadafb4a5dfb73bd320
Clinton Rodrigues <clintonrodrigues.dev@gmail.com>
2024-10-31 20:04:42 -0300

    add folder and files

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'controllers/product_controller.dart';
+import 'controllers/category_controller.dart';
+import 'screens/product_list_screen.dart';
+import 'screens/category_list_screen.dart'; // Importe a tela de categorias
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ProductController()),
+        ChangeNotifierProvider(create: (_) => CategoryController()),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello World App'),
-        ),
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+      title: 'Product App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: HomeScreen(), // Use o HomeScreen como tela inicial
     );
   }
 }
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product App'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text(
+                'Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Produtos'),
+              onTap: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => ProductListScreen()),
+                );
+              },
+            ),
+            ListTile(
+              title: Text('Categorias'),
+              onTap: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => CategoryListScreen()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      body: ProductListScreen(), // Tela padrão inicial
+    );
+  }
+}
+
